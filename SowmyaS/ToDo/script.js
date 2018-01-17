8d094ef6210f226835d00410a450d7f5d2e543a5
SowmyaSeshadri <seshadrisowmya5@gmail.com>
2018-01-17 10:37:43 +0530

    Final Changes after Review

--- SowmyaS/ToDo/script.js
+++ SowmyaS/ToDo/script.js
@@ -1,83 +1,88 @@
 var todos = new Array();
 var count = 100;
 var window = window;
+var todosUl = window.document.getElementById('ul');
+var fragment = window.document.createDocumentFragment();
+
 //Constructor to add each item
 function addTodo(todoText){
     this.todoText = todoText;
     this.delete =  false;
-    this.id= ++count;
+    this.id= count;
 }
 
-var todoList = {
-    deleteTodo: function (position) {
-        for(var i=0;i<todos.length;i++){
-            if(todos[i].id == position)
-                todos.splice(i, 1);
-        }
-    }
-};
-
-
+//Handlers for functions
 var handlers = {
     addTodo: function () {
         var addTodoTextInput = window.document.getElementById('addTodoTextInput');
-        var space = '^\\s+$';
-        if (addTodoTextInput.value == '' || addTodoTextInput.value == null || addTodoTextInput.value.match(space)) 
+        if (addTodoTextInput.value == false) 
         {
-            var doc = window.document.getElementById('addTodoTextInput');
-            doc.classList.add('focus');
+            addTodoTextInput.classList.add('focus');
             window.alert('Empty field');
-            doc.classList.remove('focus');
+            addTodoTextInput.classList.remove('focus');
         }
         else {
             todos.push(new addTodo(addTodoTextInput.value));
+            todoList.addTodosToPage(-1, addTodoTextInput.value);
             addTodoTextInput.value = '';
-            addTodosToPage();
         }
     },
     deleteTodo: function (position) {
         todoList.deleteTodo(position);
-        addTodosToPage();
     }
 };
 
+var todoList = {
+    deleteTodo: function (position) {
+     var idOfDiv = window.document.querySelector('div[data-id="'+position+'"]').remove();
+      for(var i=0 ; i<todos.length ; i++){
+            if(todos[i].id == position)
+                todos.splice(i, 1);
+        }
+        console.log(todos);
+    },
+    addTodosToPage: function(position,todos){
+        if(position >= 0){
+        for(var i=position ;i<todos.length;i++){
+            var todoItem = todos[i];
+            view.createUIItem(todoItem);   
+           todosUl.appendChild(fragment);
+        }
+    } 
+        else {
+            view.createUIItem(todos);
+            todosUl.insertBefore(fragment,todosUl.childNodes[0]);
+        }
+    } 
+};
+
+//Create an element in the UI 
 var view = {
     createDelButton: function () {
         var delButton = window.document.createElement('button');
         delButton.textContent = 'Delete';
         delButton.className = 'deleteButton';
+        delButton.setAttribute("data-delete-id",count);
         return delButton;
     },
     createStrikeButton: function () {
         var strikeButton = window.document.createElement('button');
-        strikeButton.textContent = 'Strike';
+        strikeButton.textContent = 'Completed';
         strikeButton.className = 'strikeButton';
         return strikeButton;
     },
-    setUpEventListeners: function () {
-        var todoUl = window.document.querySelector('ul');
-        todoUl.addEventListener('click', function (event) {
-            event.target.parentNode.classList.add('strikethrough');
-            var elementClicked = event.target;
-            if (elementClicked.className === 'deleteButton') {
-                var contentToDel = elementClicked.parentNode.firstChild.textContent;
-                var confirmDel = window.confirm('Delete item '+contentToDel+'?');
-                if(confirmDel == true){
-                    var parent = event.target.parentNode;
-                    var parId= parent.getAttribute('data-id');
-                    handlers.deleteTodo(parId);
-                }
-            }
-            if(elementClicked.className === 'strikeButton'){
-                var del = event.target.parentNode.firstChild;
-                del.classList.toggle('strikethrough');
-                event.target.parentNode.classList.remove('strikethrough');
-            }
-        });
+    createUIItem: function(todoItem){
+        var li = window.document.createElement('li');
+        var todoDiv = window.document.createElement('div');
+        var id = count++;
+        todoDiv.setAttribute('data-id',''+count+'');
+        fragment.appendChild(todoDiv);
+        todoDiv.appendChild(li);
+        todoDiv.appendChild(view.createDelButton());
+        todoDiv.appendChild(view.createStrikeButton());
+        li.innerHTML = todoItem.title || todoItem.todoText || todoItem;  
     }
 };
-view.setUpEventListeners();
-
 
 //Using AJAX REQUEST to get from the JSON 
 var XMLHttpRequest = XMLHttpRequest;
@@ -87,8 +92,9 @@ window.document.getElementById('retrieveData').addEventListener('click', functio
     request.onreadystatechange = function(){
         if(this.readyState == this.DONE && this.status ==200){
             if(this.responseText){
-                parseTodoItems(this.responseText); //calling function
-                addTodosToPage();
+                var n=todos.length;
+                parseTodoItems(this.responseText);
+                todoList.addTodosToPage(n, todos);
             }
         }
     };
@@ -109,26 +115,27 @@ function parseTodoItems(todoJSON){
     }
 }
 
-function addTodosToPage(){
-    var todosUl = window.document.getElementById('ul');
-    var fragment = window.document.createDocumentFragment();
-    todosUl.innerHTML = ' ';
-    for(var i=0 ;i<todos.length;i++){
-        var todoItem = todos[i];
-        createUiElement(todoItem,fragment,todosUl);       
-    } 
+
+function setUpEventListeners() {
+    var todoUl = window.document.querySelector('ul');
+    todoUl.addEventListener('click', function (event) {
+        var elementClicked = event.target;
+        if (elementClicked.className === 'deleteButton') {
+            var contentToDel = elementClicked.parentNode.firstChild.textContent;
+            var confirmDel = window.confirm('Delete item '+contentToDel+'?');
+            if(confirmDel == true){
+                var buttonPlace = event.target;
+                var buttonId= buttonPlace.getAttribute('data-delete-id');
+                handlers.deleteTodo(buttonId);
+            }
+        }
+        if(elementClicked.className === 'strikeButton'){
+            var del = event.target.parentNode.firstChild;
+            del.classList.toggle('strikethrough');
+            event.target.parentNode.classList.remove('strikethrough');
+        }
+    });
 }
 
-function createUiElement(todoItem,fragment,todosUl){
-    var li = window.document.createElement('li');
-    var todoDiv = window.document.createElement('div');
-    var id = todoItem.id;
-    todoDiv.setAttribute('data-id',''+id+'');
-    fragment.appendChild(todoDiv);
-    todoDiv.appendChild(li);
-    todoDiv.appendChild(view.createDelButton());
-    todoDiv.appendChild(view.createStrikeButton());
-    li.innerHTML = todoItem.title || todoItem.todoText || todoItem;
-    todosUl.appendChild(fragment);
-}
+setUpEventListeners();
 
